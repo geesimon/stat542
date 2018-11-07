58b65f092c26c4a6aaefdca036444f1e84b7b8ca
geesimon <geesimon@hotmail.com>
2018-11-07 09:46:17 +0800

    Archived 1596.564

--- project2/mymain.R
+++ project2/mymain.R
@@ -150,7 +150,7 @@ tbats_forecast <- function(train.data, test.data){
 #forecast.functions = c(snaive_forecast, nnetar_forecast, tbats_forecast)
 #forecast.functions = c(regression_forecast)
 # forecast.functions = c(arima_forecast)
-forecast.functions = c(naive_forecast, naive_forecast, dynamic_forecast)
+forecast.functions = c(naive_forecast, snaive_forecast, dynamic_forecast)
 
 mypredict <- function() {
   ###### Create train and test time-series #######
