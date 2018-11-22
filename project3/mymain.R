0cb14c9e05efa3f7deacf7a03a17975ffd678823
geesimon <geesimon@hotmail.com>
2018-11-22 20:56:44 +0800

    Done first DCA

--- project3/mymain.R
+++ project3/mymain.R
@@ -10,7 +10,6 @@ pacman::p_load(
   "kernlab"
 )
 
-
 #########################################################################
 # log-loss function
 logLoss = function(y, p){
@@ -36,17 +35,75 @@ convert_label <- function(train.data, test.data){
   list(train = train.data, test = test.data)
 }
 
-handle_missing <- function(train.data, test.data){
+fill_NA_by_vast <- function(train.column, test.column) {
+  stats = sort(summary(train.column), decreasing = TRUE)
   
+  train.column[is.na(train.column)] = names(stats)[1]
+  test.column[is.na(test.column)] = names(stats)[1]
+  
+  list(train = train.column, test = test.column)
+}
+
+fill_NA_by_others <- function(train.column, test.column){
+  levels(train.column) = c(levels(train.column), "MyOthers")
+  test.column = factor(test.column, levels = levels(train.column))
+  
+  train.column[is.na(train.column)] = "MyOthers"
+  test.column[is.na(test.column)] = "MyOthers"
+  
+  list(train = train.column, test = test.column)
+}
+
+fill_NA_by_mean <- function(train.column, test.column){
+  avg = mean(train.column[!is.na(train.column)])
+  train.column[is.na(train.column)] = avg
+  test.column[is.na(test.column)] = avg
+
+  list(train = train.column, test = test.column)
+}
+
+fill_NA_by_zero <- function(train.column, test.column){
+  train.column[is.na(train.column)] = 0
+  test.column[is.na(test.column)] = 0
+  
+  list(train = train.column, test = test.column)
+}
+
+handle_missing <- function(train.data, test.data){
+  # r = fill_NA_by_others(train.data[, "emp_title"], test.data[, "emp_title"])
+  # train.data[, "emp_title"] = r$train
+  # test.data[, "emp_title"] = r$test
+  
+  r = fill_NA_by_others(train.data[, "emp_length"], test.data[, "emp_length"])
+  train.data[, "emp_length"] = r$train
+  test.data[, "emp_length"] = r$test
+  
+  r = fill_NA_by_mean(train.data[, "revol_util"], test.data[, "revol_util"])
+  train.data[, "revol_util"] = r$train
+  test.data[, "revol_util"] = r$test
+  
+  r = fill_NA_by_mean(train.data[, "dti"], test.data[, "dti"])
+  train.data[, "dti"] = r$train
+  test.data[, "dti"] = r$test
+  
+  r = fill_NA_by_mean(train.data[, "mort_acc"], test.data[, "mort_acc"])
+  train.data[, "mort_acc"] = r$train
+  test.data[, "mort_acc"] = r$test
+  
+  r = fill_NA_by_zero(train.data[, "pub_rec_bankruptcies"], test.data[, "pub_rec_bankruptcies"])
+  train.data[, "pub_rec_bankruptcies"] = r$train
+  test.data[, "pub_rec_bankruptcies"] = r$test
   
   list(train = train.data, test = test.data)
 }
 
 remove_variables <- function(train.data, test.data){
-  train.name = c('id','sub_grade', 'emp_title', 'emp_length', 'title', 
-                 'zip_code', 'addr_state', 'earliest_cr_line')
-  test.name = c('sub_grade', 'emp_title', 'emp_length', 'title', 
-               'zip_code', 'addr_state', 'earliest_cr_line')
+  # train.name = c('id','sub_grade', 'emp_title', 'title',
+  #                'zip_code', 'addr_state', 'earliest_cr_line')
+  # test.name = c('sub_grade', 'emp_title', 'title',
+  #              'zip_code', 'addr_state', 'earliest_cr_line')
+  train.name = c('id','emp_title', 'title', 'zip_code', 'earliest_cr_line')
+  test.name = c('emp_title', 'title', 'zip_code', 'earliest_cr_line')
   
   train.data = train.data[, !colnames(train.data) %in% train.name]
   test.data = test.data[, !colnames(test.data) %in% test.name]
@@ -54,12 +111,49 @@ remove_variables <- function(train.data, test.data){
   list(train = train.data, test = test.data)
 }
 
+group_level <- function(train.column, test.column, group_count) {
+  groups = list()
+  stats = sort(summary(train.column), decreasing = TRUE)
+  level.names = names(stats)
+  level.interval = floor(sum(stats) / group_count)
+  
+  acc = 0
+  group_count = 1
+  item_names = c()
+  for (i in 1:length(stats)){
+    item_names = c(item_names, level.names[i])
+    acc = acc + stats[i]
+    if( acc > level.interval){
+      groups[[group_count]] = item_names
+      group_count = group_count + 1
+      item_names = c()
+      acc = 0
+    }
+  }
+  
+  groups
+}
+
+add_features <- function(train.data, test.data){
+  #Change "earliest_cr_line" as month till 2019-1-1
+  till = as.Date("1-1-2019", "%d-%m-%Y")
+  
+  full_date = paste("1-",train.data$earliest_cr_line, sep = "")
+  train.data$earliest_cr_line_mon = floor((till - as.Date(full_date, "%d-%b-%Y")) / 30)
+  
+  full_date = paste("1-",test.data$earliest_cr_line, sep = "")
+  test.data$earliest_cr_line_mon = floor((till - as.Date(full_date, "%d-%b-%Y")) / 30)
+  
+  list(train = train.data, test = test.data)
+}
+
 preprocess_data <- function(train.data, test.data){
   r = convert_label(train.data, test.data)
-  r = remove_variables(r$train, r$test)
+  r = add_features(r$train, r$test)
   r = handle_missing(r$train, r$test)
-  
-  r
+  r = remove_variables(r$train, r$test)
+
+  return (r)
 }
 
 logreg_predict = function(train.data, test.data){
@@ -112,13 +206,19 @@ xgb_predict = function(train.data, test.data) {
   predict(xgb_model, X_test)
 }
 
-train_predict = function(train.data, test.data, model.func, output.filename){
+train_predict = function(train.data, test.data, label.data, model.func, output.filename){
   pred = model.func(train.data, test.data)
   
   output = cbind(test.data$id, round(pred, 2))
   colnames(output) = c("id", "prob")
   
   write.csv(output, output.filename, row.names = FALSE, quote = FALSE)
+  
+  if(!is.null(label.data)){
+    return (logLoss(label.data$y, pred))
+  } else {
+    return (0)
+  }
 }
 
 #######################################
@@ -126,13 +226,18 @@ train_predict = function(train.data, test.data, model.func, output.filename){
 #######################################
 set.seed(6682)
 
-TRAIN_FILE_NAME = "train1.csv"
-TEST_FILE_NAME = "test1.csv"
-LABEL_FILE_NAME = "label1.csv"
-
+if (!exists("TRAIN_FILE_NAME")) {
+  TRAIN_FILE_NAME = "train.csv"
+  TEST_FILE_NAME = "test.csv"
+}
 train.data = read.csv(TRAIN_FILE_NAME)
 test.data = read.csv(TEST_FILE_NAME)
-label.data = read.csv(LABEL_FILE_NAME)
+
+if (exists("LABEL_FILE_NAME")){
+  label.data = read.csv(LABEL_FILE_NAME)
+} else {
+  label.data = NULL
+}
 
 output_filenames = c("mysubmission1.txt", "mysubmission2.txt", "mysubmission3.txt")
 
@@ -146,5 +251,6 @@ model_functions = list(
 r = preprocess_data(train.data, test.data)
 
 for (f in 1:length(model_functions)) {
-  train_predict(r$train, r$test, model_functions[[f]], output_filenames[f])
+  logloss = train_predict(r$train, r$test, label.data, model_functions[[f]], output_filenames[f])
+  cat("Model:", names(model_functions)[f], "\t LogLoss=", logloss, "\n")
 }
