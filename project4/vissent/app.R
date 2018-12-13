b3849da912d024d813fba511bbc1515b2fd1ce77
geesimon <geesimon@hotmail.com>
2018-12-13 19:08:43 +0800

    working on UI
    Former-commit-id: fe8a4cc3d52348e036dc290836925f0ac3a7764d

--- project4/vissent/app.R
+++ project4/vissent/app.R
@@ -3,23 +3,18 @@ library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   theme = "vissent.css",
-   titlePanel("IMDB Movie Review Sentiment Analysis"),
-   textAreaInput("text", "Plesae input your text:"),
-   actionButton(inputId = "predict", label = "Evaluate"),
-   plotOutput("distPlot")
+  titlePanel("IMDB Movie Review Sentiment Analysis"),
+  textAreaInput("review_content", "Please input the movie review:", width = '400px', height = '200px'),
+  actionButton(inputId = "predict", label = "Evaluate"),
+  htmlOutput("marked_review")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   observeEvent(input$predict, {print(input$clicks)})
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      # x    <- faithful[, 2] 
-      # bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      # 
-      # # draw the histogram with the specified number of bins
-      # hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  review_text = eventReactive(input$predict, input$review_content)
+  observeEvent(input$predict, {
+      output$marked_review <- renderUI(tags$h1(review_text()))
+    })
 }
 
 # Run the application 
